21726a899badfa881c39e7c1d5309795f6ad6b4f
Glen Dobson <g.dobson@lancaster.ac.uk>
2025-03-21 11:44:02 +0000

    Attempt to set vite base url for production build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
+const BASE_URL = process.env.ASSET_URL || ''
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
@@ -13,4 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  base: `${BASE_URL}`,
 })
