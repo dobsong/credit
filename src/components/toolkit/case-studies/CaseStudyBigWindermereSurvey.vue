f633cf02ccaa7df42c70f273c5d4ef77356fa637
Glen Dobson <g.dobson@lancaster.ac.uk>
2025-09-08 16:19:47 +0100

    Updated content for Windermere Case Study

--- src/components/toolkit/case-studies/CaseStudyBigWindermereSurvey.vue
+++ src/components/toolkit/case-studies/CaseStudyBigWindermereSurvey.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ToolkitHeading from '@/components/ui/ToolkitHeading.vue'
 import ToolkitReference from '@/components/ui/ToolkitReference.vue'
 import ToolkitSection from '@/components/ui/ToolkitSection.vue'
 import { scrollToSection } from '@/utility'
@@ -12,23 +13,17 @@ import Button from '@/volt/Button.vue'
     <ToolkitSection section-id="background" css-class="" :slide-in-direction="1">
       <div class="grid grid-cols-3 h-full pt-4 md:pb-64">
         <div class="col-span-3 md:col-span-2 my-auto">
-          <h2 class="font-[MuseoSans] font-extrabold md:mb-6 text-center">
-            <span class="text-6xl md:text-7xl lg:text-9xl align-middle">T</span>
-            <span class="text-3xl md:text-4xl lg:text-6xl align-middle">he </span>
-            <span class="text-6xl md:text-7xl lg:text-9xl align-middle">B</span>
-            <span class="text-3xl md:text-4xl lg:text-6xl align-middle">ig </span>
-            <span class="text-6xl md:text-7xl lg:text-9xl align-middle">W</span>
-            <span class="text-3xl md:text-4xl lg:text-6xl align-middle">indermere </span>
-            <span class="text-6xl md:text-7xl lg:text-9xl align-middle">S</span>
-            <span class="text-3xl md:text-4xl lg:text-6xl align-middle">urvey</span>
-          </h2>
+          <ToolkitHeading text="The Big Windermere Survey"></ToolkitHeading>
           <p class="md:mb-4 text-base md:text-xl lg:mx-24">
-            <a href="https://www.fba.org.uk/the-big-windermere-survey" target="_blank"
-              >The Big Windermere Survey</a
-            >
-            was launched jointly by the Freshwater Biological Association (FBA) and Lancaster
-            University, using citizen scientists to help assess the environmental health of
-            Windermere.
+            <a href="https://www.fba.org.uk/the-big-windermere-survey" target="_blank">
+              The Big Windermere Survey
+            </a>
+            (BWS) is an innovative and impactful citizen science project designed to assess the
+            environmental health of Windermere, a vital and iconic body of water within the Lake
+            District National Park and UNESCO World Heritage site. The project was first developed
+            in 2022 through collaboration between Lancaster University and the Freshwater Biological
+            Association (FBA), in response to community concern around water quality and gaps in
+            traditional water quality monitoring.
           </p>
           <ul class="md:w-4/5 lg:w-3/5 mx-auto list-disc md:mb-8 text-base md:text-xl">
             <li>
@@ -41,9 +36,10 @@ import Button from '@/volt/Button.vue'
             </li>
             <li>
               <a
-                href="https://lovewindermere.co.uk/latest-news/second-big-windermere-survey/"
+                href="https://www.lancaster.ac.uk/rse/news/windermere-two-year-report-demands-action-to-address-poor-water-quality-results"
                 target="_blank"
-                >Second Big Windermere Survey - Love Windermere
+              >
+                Windermere two-year report demands action to address ‘Poor’ water quality results
               </a>
             </li>
           </ul>
@@ -85,39 +81,20 @@ import Button from '@/volt/Button.vue'
         </div>
         <div class="my-auto md:w-1/2 lg:p-8">
           <p class="mb-1 md:mb-4 text-base md:text-xl">
-            The project previously received funding from Lancaster University’s Vice-Chancellor’s
-            Prize for Participatory Research (2021-2022), and from United Utilities through the
-            <a href="https://lovewindermere.co.uk/about-us/" target="_blank">Love Windermere</a>
-            Partnership.
-            <a href="https://www.fba.org.uk/" target="_blank">
-              The Freshwater Biological Association
-            </a>
-            has been a partner in the Love Windermere Partnership. The survey has also been
-            supported by the
-            <a href="https://www.ceh.ac.uk/" target="_blank">
-              UK Centre for Ecology and Hydrology
-            </a>
-            ,
-            <a href="https://www.lakedistrict.gov.uk/" target="_blank">
-              Lake District National Park Authority
-            </a>
-            ,
-            <a href="https://scrt.co.uk/" target="_blank">South Cumbria Rivers Trust</a>
-            ,
-            <a href="https://www.brathay.org.uk/" target="_blank">Brathay Trust</a>
-            ,
-            <a href="https://www.nationaltrust.org.uk/" target="_blank">National Trust</a>
-            and the
-            <a href="https://www.gov.uk/government/organisations/environment-agency" target="_blank"
-              >Environment Agency</a
-            >.
+            The project was initially funded by Lancaster University’s Vice-Chancellor’s Prize for
+            Participatory Research (2021-2022). Subsequent financial support for the project has
+            been kindly provided by Aviva Community Fund, Environment Agency, Lake District
+            Foundation, Lake District National Park Authority, Lakeland, Lancaster University,
+            National Trust, United Utilities, Westmorland and Furness Council, and private
+            donations. The BWS has also been supported by the UK Centre for Ecology & Hydrology,
+            South Cumbria Rivers Trust, and Brathay Trust.
           </p>
         </div>
       </div>
-      <div class="w-full text-center md:pt-8">
-        <Button @click="scrollToSection('approach')" class="text-sm md:text-base"
-          >Next: Approach</Button
-        >
+      <div class="w-full text-center md:pt-8 md:mb-24">
+        <Button @click="scrollToSection('approach')" class="text-sm md:text-base">
+          Next: Approach
+        </Button>
       </div>
     </ToolkitSection>
 
@@ -138,40 +115,69 @@ import Button from '@/volt/Button.vue'
             <span class="text-3xl md:text-4xl lg:text-6xl align-middle">pproach</span>
           </h2>
           <p class="`pb-2">
-            The survey used citizen scientists to gather water samples to provide one-day snapshots
-            of conditions in Windermere. Citizen scientists were trained to collect water samples
-            from approximately 100 different locations on Windermere and in the rivers and lakes
-            that flow into it. The samples were analysed for nutrient and bacterial concentrations
-            at independent laboratories.
+            Ten surveys have been conducted seasonally (Spring, Summer, Autumn and Winter) over two
+            and a half years between June 2022 and November 2024. During a two-hour window in each
+            survey, dedicated citizen scientists collect water samples from over 100 locations in
+            the catchment of Windermere. The citizen scientists receive training via videos and
+            printed guides, and are provided with the same sampling equipment used in water quality
+            research, ensuring robust sample collection. Samples collected during each survey are
+            analysed for over 15 water quality parameters at Lancaster University research
+            laboratories and at external accredited laboratories. Over 350 volunteer citizen
+            scientists have contributed their time to this unprecedented sampling campaign,
+            generating a unique dataset based on over 1000 water samples. No traditional academic
+            research or monitoring programme would be capable of collecting samples at the same
+            spatial intensity as the BWS.
           </p>
           <p>
             <ToolkitReference
-              authors="Harper, Lynsey R., Kelly, Emma, Bregstein, Trine, Johnson, Simon, Lavictoire, Louise & Surridge, Ben W. J."
-              :year="2023"
-              url="https://www.ceh.ac.uk/sites/default/files/2024-02/SFG109-Lynsey-Big-Windermere-Survey.pdf"
-              title="The Big Windermere Survey - A Citizen
-            Science Approach To Monitoring Water Quality"
-              citation="Lynsey R. Harper, Emma Kelly, Trine Bregstein, Simon Johnson, Louise Lavictoire & Ben W. J. Surridge, The Big Windermere Survey - A Citizen Science Approach To Monitoring Water Quality, BWS Summary Report August 2023"
+              authors="Surridge, B.W.J., Cowling, S., Lavictoire, L., Kelly, E., Johnson, S."
+              :year="2025"
+              url="https://www.fba.org.uk/fba-voice/windermere-two-year-report-demands-action-to-address-poor-water-quality-results"
+              title="Big Windermere Survey Summary Report, June 2022 - November 2024"
+              citation="Surridge, B.W.J, Cowling, S., Lavictoire, L., Kelly, E., Johnson, S. (2025) Big Windermere Survey Summary Report, June 2022 - November 2024. Freshwater Biological Association, UK."
             ></ToolkitReference>
           </p>
-          <div class="text-base">(Summary report - August 2023)</div>
-          <div
-            class="bg-amber-200 w-64 rounded-xl drop-shadow-xl drop-shadow-slate-500 text-center mx-auto my-4"
-          >
-            <span class="pi pi-exclamation-circle float-right p-2"></span>
-            <h3 class="font-bold pt-4">Challenges</h3>
-            <p class="px-4 pb-8 pt-4">Sourcing funding for continuation.</p>
-          </div>
           <div class="text-center md:pt-8">
-            <Button @click="scrollToSection('outcomes')" class="text-sm md:text-base">
-              Next: Outcomes & Impact
+            <Button @click="scrollToSection('challenges')" class="text-sm md:text-base">
+              Next: Challenges
             </Button>
           </div>
         </div>
       </div>
     </ToolkitSection>
 
-    <ToolkitSection section-id="outcomes" :slide-in-direction="-1" css-class="lg:h-[90svh]">
+    <ToolkitSection section-id="challenges" :slide-in-direction="1" css-class="">
+      <div class="grid grid-cols-1 md:w-2/3 mx-auto">
+        <div>
+          <h2
+            class="md:w-full font-[MuseoSans] font-extrabold md:mb-6 text-center dark:text-[var(--color-analysis)]"
+          >
+            <span class="text-6xl md:text-7xl lg:text-9xl align-middle">C</span>
+            <span class="text-3xl md:text-4xl lg:text-6xl align-middle">hallenges</span>
+          </h2>
+        </div>
+
+        <ul class="list-disc pl-6 mb-4 text-base md:text-xl">
+          <li>
+            Managing expectations among citizen scientists regarding the extent to which the project
+            itself can lead changes in the catchment that improve water quality.
+          </li>
+          <li>
+            Maintaining the independence (real and perceived) of the research project when it is
+            located in the middle of a politically and publicly charged issue such as water
+            pollution.
+          </li>
+          <li>Securing funding for continuation of the project.</li>
+        </ul>
+      </div>
+      <div class="w-full text-center md:pt-8">
+        <Button @click="scrollToSection('outcomes')" class="text-sm md:text-base">
+          Next: Outcomes & Impact
+        </Button>
+      </div>
+    </ToolkitSection>
+
+    <ToolkitSection section-id="outcomes" :slide-in-direction="1" css-class="lg:h-[90svh]">
       <div
         class="lg:w-2/3 grid grid-cols-1 h-full pt-4 text-lg md:text-xl mx-auto my-auto lg:pb-32"
       >
@@ -188,17 +194,23 @@ import Button from '@/volt/Button.vue'
         <div class="col-span-2 md:col-span-1 mx-4">
           <ul class="list-disc pl-4">
             <li>
-              Open Access Data in the form of summary reports available at
-              <a href="https://www.fba.org.uk/bws-results" target="_blank"
-                >BWS Results | Freshwater Biological Association</a
+              All data and summary reports are open access and made freely and openly available to
+              all interested parties via:
+              <a href="https://www.fba.org.uk/bws-results" target="_blank">
+                BWS Results | Freshwater Biological Association </a
               >, and interactive maps on
               <a
                 href="https://app.cartographer.io/embed/map?map=FbaWindermere&workspace=windermere&project=windermere"
                 target="_blank"
-                >Cartographer</a
+              >
+                Cartographer</a
               >.
             </li>
-            <li>Data feeds into decision making of the Love Windermere partnership.</li>
+            <li>
+              The BWS project team continues to engage with many partners in the catchment,
+              including as the Love Windermere Partnership, to ensure BWS data influence policy and
+              practice related to water quality in Windermere
+            </li>
           </ul>
         </div>
       </div>
